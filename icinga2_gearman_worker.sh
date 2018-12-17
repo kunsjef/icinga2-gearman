d8669dcfebd426b5421768bb330e607347d1837d
Thomas Vådahl <thomas@fellowship.no>
2018-12-17 13:51:26 +0100

    Update and rename icinga2-gearman-worker.sh to icinga2_gearman_worker.sh

--- icinga2_gearman_worker.sh
+++ icinga2_gearman_worker.sh
@@ -2,7 +2,7 @@
 
 # Icinga2 gearman worker
 # ------------------------------------------------------
-#    version         icinga2-gearman-worker.sh 0.4
+#    version         icinga2_gearman_worker.sh 0.4
 #    author          Thomas Vadahl
 #    copyright       Copyright (c) Thomas Vadahl
 #    license         GNU General Public License
@@ -10,14 +10,14 @@
 # Description:
 # The point of this script is to join a gearman job server queue and perform
 # checks for Icinga2. The expected result is the normal check result from any
-# Icinga/Nagios chec PLUS the exitstatus. This exitstatus needs to be read by
+# Icinga/Nagios check PLUS the exitstatus. This exitstatus needs to be read by
 # the gearman_check.sh script.
 #
 # Run like this:
-# gearman -w -h <gearman_job_server_hostname> -p <port> -f <queue-name> xargs ./icinga2-gearman-worker.sh
+# gearman -w -h <gearman_job_server_hostname> -p <port> -f <queue-name> xargs ./icinga2_gearman_worker.sh
 #
 # Example:
-# gearman -w -h 10.0.0.1 -p 4730 -f icinga2 xargs ./icinga2-gearman-worker.sh
+# gearman -w -h 10.0.0.1 -p 4730 -f icinga2 xargs ./icinga2_gearman_worker.sh
 
 path="/usr/lib/nagios/plugins"
 command="$@"
